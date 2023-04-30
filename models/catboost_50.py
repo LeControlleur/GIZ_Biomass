f37c83d1a361fb8de61307c6224d348ba432929c
LeControlleur <sammy.bini@yahoo.fr>
2023-04-30 23:17:58 +0000

    :recycle:Code refactoring for modularity

--- models/catboost_50.py
+++ models/catboost_50.py
@@ -4,31 +4,38 @@ from catboost import CatBoostRegressor
 
 if __name__=="__main__":
 
-    BiomassPreprocesser.load_data()
+	BiomassPreprocesser.load_data()
+	
+	train_images = BiomassPreprocesser.train_images
+	train_biomasses = BiomassPreprocesser.train_biomasses
+	validate_images = BiomassPreprocesser.validate_images
+	validate_biomasses = BiomassPreprocesser.validate_biomasses
+	test_images = BiomassPreprocesser.test_images
+	test_biomasses = BiomassPreprocesser.test_biomasses
+	
+	BiomassPreprocesser.features_selection()
+	print(BiomassPreprocesser.MEAN.shape)
+    
+	print("Data transformation...")
+	train_images_transformed = BiomassPreprocesser.data_transformer(
+		train_images
+	)
 
-    train_images = BiomassPreprocesser.train_images
-    train_biomasses = BiomassPreprocesser.train_biomasses
-    validate_images = BiomassPreprocesser.validate_images
-    validate_biomasses = BiomassPreprocesser.validate_biomasses
-    test_images = BiomassPreprocesser.test_images
-    test_biomasses = BiomassPreprocesser.test_biomasses
-
-    BiomassPreprocesser.features_selection()
-
-    train_images_transformed = BiomassPreprocesser.data_transformer(train_images)
-
-    params = {'iterations': [50, 100, 200],
+	params = {'iterations': [100],
+        #       'iterations': [50, 100, 200],
             'depth': [8, 15, 25],
             'l2_leaf_reg': [0.2, 0.8, 3]}
-
-    catboost_model = CatBoostRegressor(loss_function='RMSE')
+	
+	print("Catboost regression application...")
+	catboost_model = CatBoostRegressor(loss_function='RMSE')
     
-    # Train the grid search model
-    catboost_model.grid_search(param_grid=params, 
+	# Train the grid search model
+	catboost_model.grid_search(param_grid=params, 
                     X=train_images_transformed, 
                     y=train_biomasses, 
                     verbose=True, 
                     plot=True)
-    
-    catboost_model.save_model("models/model", )
+	
+	print("Training end, saving model ...")
+	catboost_model.save_model("model_files/model_100", format="onnx")
     
--- models/preprocessing.py
+++ models/preprocessing.py
@@ -33,7 +33,8 @@ class CustomScaler(BaseEstimator, TransformerMixin):
 
   def reverse_transform(self):
     return None
-    
+
+
 class FlattenTransformer(BaseEstimator, TransformerMixin):
 
   def fit(self, X, y=None):
@@ -52,10 +53,13 @@ class BiomassPreprocesser:
     test_images : np.array = None
     test_biomasses : np.array = None
     selected_features : np.array = None
+    MEAN = None
+    STD = None
+
 
     def load_train_data() -> tuple[np.array, np.array]:
         print("Train data loading ....")
-        trainset = h5py.File("../../drive/MyDrive/vscode-ssh/GIZ_Biomass/data/09072022_1154_train.h5", "r")
+        trainset = h5py.File("/content/drive/MyDrive/vscode-ssh/GIZ_Biomass/data/09072022_1154_train.h5", "r")
         train_images = np.array(trainset['images'],dtype=np.float64)
         train_images = train_images.transpose(0,3,1,2)
         train_biomasses = np.array(trainset['agbd'],dtype=np.float64)
@@ -69,7 +73,7 @@ class BiomassPreprocesser:
     def load_validate_data() -> tuple[np.array, np.array]:
 
         print("Validation data loading ....")
-        validateset = h5py.File("../../drive/MyDrive/vscode-ssh/GIZ_Biomass/data/09072022_1154_val.h5", "r")
+        validateset = h5py.File("/content/drive/MyDrive/vscode-ssh/GIZ_Biomass/data/09072022_1154_val.h5", "r")
         validate_images = np.array(validateset['images'],dtype=np.float64)
         validate_images = validate_images.transpose(0,3,1,2)
         validate_biomasses = np.array(validateset['agbd'],dtype=np.float64)
@@ -83,13 +87,13 @@ class BiomassPreprocesser:
     def load_test_data() -> tuple[np.array, np.array]:
 
         print("Test data loading ....")
-        testset = h5py.File("../../drive/MyDrive/vscode-ssh/GIZ_Biomass/data/09072022_1154_test.h5", "r")
+        testset = h5py.File("/content/drive/MyDrive/vscode-ssh/GIZ_Biomass/data/09072022_1154_test.h5", "r")
         test_images = np.array(testset['images'],dtype=np.float32)
         test_images = test_images.transpose(0,3,1,2)
         test_biomasses = np.array(testset['agbd'],dtype=np.float32)
 
-        BiomassPreprocesser.validate_images = validate_images
-        BiomassPreprocesser.validate_biomasses = validate_biomasses
+        BiomassPreprocesser.test_images = test_images
+        BiomassPreprocesser.test_biomasses = test_biomasses
 
         return test_images, test_biomasses
 
@@ -102,38 +106,47 @@ class BiomassPreprocesser:
 
     def features_selection() -> np.array:
 
-        global train_images
-        global train_biomasses
-        global selected_features
+        train_images = BiomassPreprocesser.train_images
+        train_biomasses = BiomassPreprocesser.train_biomasses
+        selected_features = BiomassPreprocesser.selected_features
 
         # constante for standardization
-        MEAN = train_images.mean((0,2,3))
-        STD = train_images.std((0,2,3))
+        BiomassPreprocesser.MEAN = train_images.mean((0,2,3))
+        BiomassPreprocesser.STD = train_images.std((0,2,3))
 
         #   Params definitions
-        params_lasso = {
-            "classifier__alpha": [1e-1],
-            "classifier" : [Lasso()]
-        }
+        # params_lasso = {
+        #     "classifier__alpha": [1e-1],
+        #     "classifier" : [Lasso(tol=1e-2)]
+        # }
 
-        pipe = Pipeline(steps=[("scaler", CustomScaler(MEAN, STD)),
+        # pipe = Pipeline(steps=[("scaler", CustomScaler(BiomassPreprocesser.MEAN, BiomassPreprocesser.STD)),
+        #                     ("flatten", FlattenTransformer()),
+        #                     ("classifier", Lasso())])
+
+        # # Train the grid search model
+        # grid_search = GridSearchCV(pipe, params_lasso, cv=3, scoring='neg_mean_squared_error', verbose=3).fit(train_images,train_biomasses)
+
+        # coefficients = grid_search.best_estimator_.named_steps['classifier'].coef_
+
+        pipe = Pipeline(steps=[("scaler", CustomScaler(BiomassPreprocesser.MEAN, BiomassPreprocesser.STD)),
                             ("flatten", FlattenTransformer()),
-                            ("classifier", Lasso())])
+                            ("classifier", Lasso(alpha=1e-1, tol=1e-2))])
 
-        # Train the grid search model
-        grid_search = GridSearchCV(pipe, params_lasso, cv=3, scoring='neg_mean_squared_error', verbose=3).fit(train_images,train_biomasses)
+        # Train the pipeline
+        pipe.fit(train_images,train_biomasses)
+        coefficients = pipe['classifier'].coef_
 
-        coefficients = grid_search.best_estimator_.named_steps['classifier'].coef_
+
         features = range(coefficients.shape[0])
         importance = np.abs(coefficients)
         selected_features = np.array(features)[importance > 0]
+        BiomassPreprocesser.selected_features = selected_features
 
         return selected_features
 
 
     def data_transformer(X : np.array, standardize : bool = True, compute_standardize_variables : bool = True, selected_features : np.array = None) -> np.array : 
-        global MEAN
-        global STD
 
         if selected_features is None:
             selected_features = BiomassPreprocesser.selected_features
@@ -145,13 +158,11 @@ class BiomassPreprocesser:
         images_resized = features_selection_layer(images_flattened, selected_features)
         if standardize :
             if compute_standardize_variables:
-                MEAN = np.mean(images_resized, axis=0)
+                BiomassPreprocesser.MEAN = np.mean(images_resized, axis=0)
             if compute_standardize_variables:
-                STD = np.std(images_resized, axis=0)
-            standardization_layer = lambda X : (X-MEAN)/STD
+                BiomassPreprocesser.STD = np.std(images_resized, axis=0)
+            standardization_layer = lambda X : (X-BiomassPreprocesser.MEAN)/BiomassPreprocesser.STD
             standardized_data = standardization_layer(images_resized)
         else :
             standardized_data = images_resized 
         return standardized_data
-
-    
